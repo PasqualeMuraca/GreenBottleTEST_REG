a1761ee5ca0b2ce36ff6eab20508ee2ec7733581
Pasquale Muraca <pasqualemuraca2003@gmail.com>
2025-01-20 23:56:49 +0100

    a

--- src/test/java/it/unisa/greenbottle/AccessoTest/RegistrazioneTest.java
+++ src/test/java/it/unisa/greenbottle/AccessoTest/RegistrazioneTest.java
@@ -13,7 +13,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultMatcher;
 
 @SpringBootTest
-
 @AutoConfigureMockMvc
 public class RegistrazioneTest {
 
